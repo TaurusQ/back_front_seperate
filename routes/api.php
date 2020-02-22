3f416b9a923814776d88a211797efd66aaee8599
TaurusQ <346248379@qq.com>
2020-02-22 16:32:36 +0800

    新增权限相关路由

--- routes/api.php
+++ routes/api.php
@@ -19,17 +19,27 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 */
 
-Route::namespace("Admin")->group(function(){
+Route::namespace("Admin")->name("api.")->prefix("admin")->group(function () {
 
-    Route::any('/test',"LoginController@test");
+    Route::any('/test', "LoginController@test");
 
     Route::post('/login', 'LoginController@login');
     Route::post('/refresh', 'LoginController@refresh');
 
     // 中间件验证 ,multiauth: guard_name
-    Route::middleware('multiauth:admin')->group(function () {
+    Route::middleware('multiauth:admin')->name("admin.")->group(function () {
         Route::post('/me', 'LoginController@me');
         Route::post('/logout', 'LoginController@logout');
-    });
 
+        // Admins
+        Route::get('/admins','AdminsController@list')->name('admins.list');
+
+        // Permissions
+        Route::get('/permissions', 'PermissionsController@list')->name('permissions.list');
+        Route::get('/permissions/{permission}', 'PermissionsController@show')->name('permissions.show');
+        Route::post('/permissions', 'PermissionsController@store')->name('permissions.store');
+        Route::patch('/permissions/{permission}', 'PermissionsController@update')->name('permissions.update');
+        Route::delete('/permissions/{permission}','PermissionsController@destroy')->name('permissions.destroy');
+        Route::delete('/permissions/batch_delete','PermissionsController@deleteAll')->name('permissions.batch_delete');
+    });
 });
